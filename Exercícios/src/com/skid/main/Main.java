9e97a6fc7ffb6971709b0fa5106e33a4f8904f9c
Gilson B <skid_novo@hotmail.com>
2016-06-22 23:04:24 -0300

    criado atributos da janela

--- Exercícios/src/com/skid/main/Main.java
+++ Exercícios/src/com/skid/main/Main.java
@@ -1,9 +1,34 @@
 package com.skid.main;
 
-public class Main {
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+	private LinhaBezier linha;
+	
+	public Main(){
+		super("Exercícios");
+		linha = new LinhaBezier();
+		setSize(WIDTH,HEIGHT);
+		setResizable(false);
+		add(linha);
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent ev){
+				linha.stopApp();
+				System.exit(1);
+			}
+		});
+		setVisible(true);
+		
+	}
 
 	public static void main(String[] args) {
-		
+		Main app = new Main();
+		app.linha.startApp();
 
 	}
 
